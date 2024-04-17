234925fed8d880b17107cc21cbbbac9abe9c7623
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-04-17 11:10:32 +0100

    fixes:Ui fixes and responsiveness fix

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,8 +1,19 @@
 <template>
-    <div>
-        <p>Hello world</p>
+   <section class="w-screen h-screen flex items-center justify-center">
+    <form class="w-11/12 md:w-1/2 xl:w-1/4">
+        <section class="my-8">
+            <h3 class="text-xl capitalize font-semibold  text-black01">Welcome back !</h3>
+            <p class="capitalize">Please input the details below to log in</p>
 
-    </div>
+        </section>
+
+        <section class="my-2 mb-2">
+            <label for="email" class="text-base02 block my-2">E-mail</label>
+            <input class="input border-[1px] border-blue01 focus:border-Till10101 focus:outline-none rounded-md w-full h-11 px-3 transition-all duration-300 ease-linear" type="email" placeholder=" Enter your email" required="" name="email" value="">
+        </section>
+
+    </form>
+   </section>
 </template>
 
 <script>
--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,27 +1,13 @@
 <template>
-  <main class="relative h-screen">
-    <!-- Logo image -->
-    <div class="absolute top-0 left-0 mt-20 ml-9">
-      <img src="../assets/icons/Gen_Code.svg" class="h-29 w-auto" alt="Logo" />
-    </div>
-
-    <!-- Back icon and text -->
-    <div class="absolute top-[15rem] ml-40 flex items-center z-10">
-      <img
-        src="../assets/icons/arrow-left.svg"
-        class="h-6 w-auto"
-        alt="Back Icon"
-      />
-      <p class="ml-2 text-base2">Back</p>
-    </div>
-
-   
-
+  <main class="w-full md:my-12 md:items-start flex flex-col overflow-x-hidden">
 
    <section class="w-screen flex items-center justify-center">
     <form class="w-11/12 md:w-1/2 xl:w-1/4">
-      <section class="my-10">
-        <h3 class="text-xl  text-black01">Register</h3>
+      <section>
+        <img src="../assets/icons/Gen_Code.svg" alt="Logo" class="h-14"/>
+      </section>
+      <section>
+        <h3 class="text-xl font-semibold  text-black01">Register</h3>
         <p class="text-sm text-Till091">Please input your details below to register</p>
 
       </section>
@@ -29,25 +15,25 @@
         <label class="text-base02 block my-2">
           First Name
         </label>
-        <input class="input border-[1px] border-Dark Till focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all duration-300 ease-linear" type="text" placeholder=" Iyanuloluwa" required="" name="Iyanuloluwa" value="">
+        <input class="input border-[1px] border-blue01 focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all px-3 duration-300 ease-linear" type="text" placeholder="Iyanuloluwa" required="" name="Iyanuloluwa" value="">
       </section>
       <section class="my-2 mb-2">
         <label class="text-base02 block my-2">
           Last Name
         </label>
-        <input class="input border-[1px] border-Dark Till focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all duration-300 ease-linear" type="text" placeholder=" last name" required="" name="Iyanuloluwa" value="">
+        <input class="input border-[1px]  border-blue01 focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all px-3 duration-300 ease-linear" type="text" placeholder="last name" required="" name="Iyanuloluwa" value="">
       </section>
       <section class="my-2 mb-2">
         <label htmlFor="username" class="text-base02 block my-2">
           Email
         </label>
-        <input class="input border-[1px] border-Dark Till focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all duration-300 ease-linear" type="text" placeholder=" email" required="" name="email" value="">
+        <input class="input border-[1px]  border-blue01 focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all px-3 duration-300 ease-linear" type="text" placeholder="email" required="" name="email" value="">
       </section>
       <section class="my-2 mb-2">
         <label htmlFor="username" class="text-base02 block my-2">
           Password
         </label>
-        <input class="input border-[1px] border-[#2E666D] focus:border-Till10101 focus:outline-none rounded-md w-full h-11 transition-all duration-300 ease-linear" type="text" placeholder=" password" required="" name="password" value="">
+        <input class="input border-[1px]  border-blue01 focus:border-Till10101 focus:outline-none rounded-md w-full h-11 px-3 transition-all duration-300 ease-linear" type="text" placeholder="password" required="" name="password" value="">
       </section>      
       <p class="text-[11px]">Password must be atleast 8 character</p>
       <section class="my-4 mb-5 w-full">
