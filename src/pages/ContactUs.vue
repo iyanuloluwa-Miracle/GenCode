e69f4ce14d65733229870395b2a65390816ee183
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-04-25 08:49:34 +0100

    feat: services page completed

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,17 +1,21 @@
 <template>
     <section>
         <Navbar/>
+        <ContactUsComponent/>
     </section>
 
    
 </template>
 
 <script>
+
     import Navbar from '../../src/components/Layout/Navbar.vue'
+    import ContactUsComponent from '../components/ContactUs/ContactUsComponent.vue';
 
     export default {
         components: {
-            Navbar
+            Navbar,
+            ContactUsComponent
         
     }
 }
