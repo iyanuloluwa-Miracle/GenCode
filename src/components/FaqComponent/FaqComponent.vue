27f30de34dbd025c3392f0c6be7889824a18a1eb
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-04-26 22:07:58 +0100

    feat: FAQ page implementation done

--- src/components/FaqComponent/FaqComponent.vue
+++ src/components/FaqComponent/FaqComponent.vue
@@ -5,16 +5,105 @@
     >
       Frequently Asked Questions
     </h2>
-    <section class="flex items-center gap-2">
+    <div class="flex items-center gap-2 mt-[-38px] ml-40">
       <img
         src="../../assets/icons/arrow-left.svg"
         alt="arrow-left"
         class="h-3"
       />
       <p class="text-[18px]">
-        <router-link to="/login">Back</router-link>
+        <router-link to="/contact-us">Back</router-link>
       </p>
-    </section>
+    </div>
+    <div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[58px]">
+            <h5 class="text-[38px] font-bold text-[#272E31]">|Q.</h5>
+            <h6 class="text-[16px] font-medium text-[#272E31]">What is a Qr code?</h6>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[10px]">
+            <h5 class="text-[38px] font-bold text-[#81B6C4]">|A.</h5>
+            <h6 class="text-[16px] text-[#272E31] ml-4">
+              A QR (Quick Response) code ia a two dimensional code that can
+              conceal thousand characters. It <br> can store any information about a
+              product or link to a destination URL. Scanning a QR code with a<br>
+              smartphone or tablet directs the user to the destination URL.
+            </h6>
+          </div>
+        </div>
+
+
+         <div>
+          <div class="flex items-center ml-[314px] mt-[58px]">
+            <h5 class="text-[38px] font-bold text-[#272E31]">|Q.</h5>
+            <h6 class="text-[16px] font-medium text-[#272E31]">How secure is a QR code??</h6>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[10px]">
+            <h5 class="text-[38px] font-bold text-[#81B6C4]">|A.</h5>
+            <h6 class="text-[16px] text-[#272E31] ml-4">
+             QR codes are 95% safe and secure to store all your data and information. 
+             QR codes are known to <br> be free from threats of hacking.   
+            </h6>
+          </div>
+        </div>
+
+
+         <div>
+          <div class="flex items-center ml-[314px] mt-[58px]">
+            <h5 class="text-[38px] font-bold text-[#272E31]">|Q.</h5>
+            <h6 class="text-[16px] font-medium text-[#272E31]">How do I  generate my QR code?</h6>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[10px]">
+            <h5 class="text-[38px] font-bold text-[#81B6C4]">|A.</h5>
+            <h6 class="text-[16px] text-[#272E31] ml-4">
+             Simply enter the required information on getcode’s web link/app,such as the destination URL and <br> generate your own dynamic QR code in the blink of an eye.
+            </h6>
+          </div>
+        </div>
+
+
+         <div>
+          <div class="flex items-center ml-[314px] mt-[58px]">
+            <h5 class="text-[38px] font-bold text-[#272E31]">|Q.</h5>
+            <h6 class="text-[16px] font-medium text-[#272E31]">Can i change the QR code later?</h6>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[10px]">
+            <h5 class="text-[38px] font-bold text-[#81B6C4]">|A.</h5>
+            <h6 class="text-[16px] text-[#272E31] ml-4">
+              If you’re worried about changing your QR codes on the long run due to any reason or simply <br> because you want to change it,yes,you can change your QR code.
+               All you have to do is change the <br> destination URL of the already generated QR code. You can also change it through your registered <br>
+               E-mail address.
+         
+            </h6>
+          </div>
+        </div>
+
+
+         <div>
+          <div class="flex items-center ml-[314px] mt-[58px]">
+            <h5 class="text-[38px] font-bold text-[#272E31]">|Q.</h5>
+            <h6 class="text-[16px] font-medium text-[#272E31]">Does the QR code have an expiry date??</h6>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center ml-[314px] mt-[10px]">
+            <h5 class="text-[38px] font-bold text-[#81B6C4]">|A.</h5>
+            <h6 class="text-[16px] text-[#272E31] ml-4">
+             Your QR codes are valid for as long as your membership is valid and active.
+            </h6>
+          </div>
+        </div>
+
+
+      </div>
   </div>
 </template>
 
